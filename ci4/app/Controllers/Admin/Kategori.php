39fd41c456baeb15d358e4a1d42397389facf935
Riskyo <68279330+Riskyo@users.noreply.github.com>
2020-11-04 16:17:19 +0700

    tugas minggu 17

--- ci4/app/Controllers/Admin/Kategori.php
+++ ci4/app/Controllers/Admin/Kategori.php
@@ -3,6 +3,7 @@
 namespace App\Controllers\Admin;
 
 use App\Controllers\BaseController;
+use App\Models\kategori_M;
 
 class kategori extends BaseController
 {
@@ -14,7 +15,15 @@ class kategori extends BaseController
 
 	public function select()
 	{
-		echo "<h1>Menampilkan semua data</h1>";
+		$model = new kategori_M();
+		$kategori = $model->findAll();
+
+		$data = [
+			'judul' => 'SELECT DATA DARI controller',
+			'kategori' => $kategori
+		];
+
+		return view("kategori/select", $data);
 	}
 
 	public function selectWhere($id = null)
@@ -24,12 +33,14 @@ class kategori extends BaseController
 
 	public function formInsert()
 	{
-		echo "<h1>Menampilkan form Insert</h1>";
+		return view("kategori/forminsert");
 	}
 
 	public function formUpdate($id = null)
 	{
-		echo "<h1>Menampilkan form Update $id</h1>";
+		echo view("template/header");
+		echo view("kategori/update");
+		echo view("template/footer");
 	}
 
 	public function update($id = null)
